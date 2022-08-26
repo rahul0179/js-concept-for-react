5142e4060fc3f5187e723efb228cb30f32e4ae52
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-27 00:03:22 +0600

    fandamental js

--- fandamenta.js
+++ fandamenta.js
@@ -1,8 +1,38 @@
 /* 1 variable */
 const fatherName = 'Arnold';
-let ses
+let season = 'rainy';
+season = 'winter';
 
+//6 basic condition: >,<,===,!==,<=,>=
+//multiple condition: && , ||
 
+if (fatherName === "arnold" || season === 'winter') {
+    console.log('if one statment true that means true')
+}
+else if (fatherName === 'Arnold') {
+    console.log('else if')
+}
+else {
+    console.log('else');
+}
+
+//3.array
+//index
+//length,push,pop
+const num = [89, 35, 98, 12]
+num[0] = 56;
+
+//for loop
+for (let i = 0; i < num.length; i++) {
+    const number = num[i];
+    console.log(number);
+}
+//function
+function multiply(n1, n2) {
+    const result = n1 + n2;
+    return result;
+}
+console.log(multiply(44, 60));
 
 
 const student = {
