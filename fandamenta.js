e49ce02f6e788e2b44ed2f7e9c5455cf9ac1b98e
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-26 23:40:38 +0600

    first commit

---
+++ fandamenta.js
@@ -0,0 +1,16 @@
+/* 1 variable */
+const fatherName = 'Arnold';
+let ses
+
+
+
+
+const student = {
+    name: 'shakib all hasan',
+    age: 32,
+    movies: ['king khan', 'dhakar mastan']
+}
+const myVariable = 'age';
+console.log(student.age) //direct by property
+console.log(student['age'])//access via property name  string
+console.log(student[myVariable])//  acces via property name in variable
